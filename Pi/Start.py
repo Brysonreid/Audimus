b94a7f53ed9b2fd94ace6cb3f306132eb7ecc61e
Bryson Reid <91795641+Brysonreid@users.noreply.github.com>
2025-04-02 14:56:21 -0400

    updates

--- Pi/Start.py
+++ Pi/Start.py
@@ -1,22 +1,50 @@
 import configparser
 import datetime
+import numpy as np
+import os
+
+
+#Get updated time from time file
+DateTimeFile = 'DateTime.txt'
+TIME = str(np.loadtxt(DateTimeFile, dtype = str))
+print(TIME)
+
+newpath = "Recordings/" +TIME
+
+#assuming a folder does not exist from this time
+#if not os.path.exists(newpath):
+    #os.makedirs(newpath)
+#else:
+    #pass
 
 
 #update new recording name
 config = configparser.ConfigParser()
 config.read('LimeSDRConf.ini')
 
-Now = datetime.datetime.now()
-NewName = "/home/pi/Audimus/Recordings/SDRdata" +Now.strftime('%Y-%m-%d-%H:%M:%S')
-print(NewName)
-config['Storage']['File']= NewName
+config['Storage']['folder'] = newpath
 
 
-with open('LimeSDRConf.ini', 'w') as configfile:
-  config.write(configfile)
+for I in range(1,6):
+    NewName = newpath+'/Section'+str(I)
+    try:
+        check = config['Storage']['file'+str(I)]
+        print("Checked")
+    except:
+        config['Storage']['file'+str(I)] = NewName
+        print(NewName)
+
+    with open('LimeSDRConf.ini', 'w') as configfile:
+      config.write(configfile)
 
 #delete recordings older than X days
 print("Running Start.py")
+
+
+
+
+
+
 
 
 
---
+++ receive_data.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#
+# SPDX-License-Identifier: GPL-3.0
+#
+# GNU Radio Python Flow Graph
+# Title: receive_data
+# Author: Bryson
+# GNU Radio version: v3.9.2.0-85-g08bb05c1
+
+from gnuradio import analog
+from gnuradio import blocks
+from gnuradio import filter
+from gnuradio.filter import firdes
+from gnuradio import gr
+from gnuradio.fft import window
+import sys
+import signal
+from argparse import ArgumentParser
+from gnuradio.eng_arg import eng_float, intx
+from gnuradio import eng_notation
+from gnuradio import soapy
+import configparser
+
+
+
+
+class receive_data(gr.top_block):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "receive_data", catch_exceptions=True)
+
+        ##################################################
+        # Variables
+        ##################################################
+        self._samp_rate_config = configparser.ConfigParser()
+        self._samp_rate_config.read('LimeSDRConf.ini')
+        try: samp_rate = self._samp_rate_config.getfloat('Radio', 'Sample_Rate')
+        except: samp_rate = 2400000
+        self.samp_rate = samp_rate
+        self._Storage_File_config = configparser.ConfigParser()
+        self._Storage_File_config.read('LimeSDRConf.ini')
+        try: Storage_File = self._Storage_File_config.get('Storage', 'file')
+        except: Storage_File = 'Recordings/SDRData'
+        self.Storage_File = Storage_File
+        self._Interpolation_config = configparser.ConfigParser()
+        self._Interpolation_config.read('LimeSDRConf.ini')
+        try: Interpolation = self._Interpolation_config.getint('Demodulation', 'Interpolation')
+        except: Interpolation = 1
+        self.Interpolation = Interpolation
+        self._Demod_Decimation_config = configparser.ConfigParser()
+        self._Demod_Decimation_config.read('LimeSDRConf.ini')
+        try: Demod_Decimation = self._Demod_Decimation_config.getint('Demodulation', 'Decimation')
+        except: Demod_Decimation = 10
+        self.Demod_Decimation = Demod_Decimation
+        self._Center_Frequency_config = configparser.ConfigParser()
+        self._Center_Frequency_config.read('LimeSDRConf.ini')
+        try: Center_Frequency = self._Center_Frequency_config.getfloat('Radio', 'Frequency')
+        except: Center_Frequency = 138000000
+        self.Center_Frequency = Center_Frequency
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.soapy_limesdr_source_0_0 = None
+        dev = 'driver=lime'
+        stream_args = ''
+        tune_args = ['']
+        settings = ['']
+
+        self.soapy_limesdr_source_0_0 = soapy.source(dev, "fc32", 1, "",
+                                  stream_args, tune_args, settings)
+        self.soapy_limesdr_source_0_0.set_sample_rate(0, samp_rate)
+        self.soapy_limesdr_source_0_0.set_bandwidth(0, 40000000)
+        self.soapy_limesdr_source_0_0.set_frequency(0, Center_Frequency)
+        self.soapy_limesdr_source_0_0.set_frequency_correction(0, 0)
+        self.soapy_limesdr_source_0_0.set_gain(0, min(max(80, -12.0), 61.0))
+        self.rational_resampler_xxx_0_0 = filter.rational_resampler_ccc(
+                interpolation=Interpolation,
+                decimation=Demod_Decimation,
+                taps=[],
+                fractional_bw=0.2)
+        self.low_pass_filter_0_0 = filter.fir_filter_ccf(
+            1,
+            firdes.low_pass(
+                1,
+                samp_rate,
+                samp_rate/4,
+                samp_rate/8,
+                window.WIN_HAMMING,
+                6.76))
+        self.blocks_head_0 = blocks.head(gr.sizeof_gr_complex*1, 57600000)
+        self.blocks_file_sink_0_0 = blocks.file_sink(gr.sizeof_gr_complex*1, Storage_File, False)
+        self.blocks_file_sink_0_0.set_unbuffered(False)
+        self.analog_agc_xx_0 = analog.agc_cc(5e-3, 1.0, 1.0)
+        self.analog_agc_xx_0.set_max_gain(65536)
+
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.analog_agc_xx_0, 0), (self.blocks_head_0, 0))
+        self.connect((self.blocks_head_0, 0), (self.blocks_file_sink_0_0, 0))
+        self.connect((self.low_pass_filter_0_0, 0), (self.rational_resampler_xxx_0_0, 0))
+        self.connect((self.rational_resampler_xxx_0_0, 0), (self.analog_agc_xx_0, 0))
+        self.connect((self.soapy_limesdr_source_0_0, 0), (self.low_pass_filter_0_0, 0))
+
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.low_pass_filter_0_0.set_taps(firdes.low_pass(1, self.samp_rate, self.samp_rate/4, self.samp_rate/8, window.WIN_HAMMING, 6.76))
+        self.soapy_limesdr_source_0_0.set_sample_rate(0, self.samp_rate)
+
+    def get_Storage_File(self):
+        return self.Storage_File
+
+    def set_Storage_File(self, Storage_File):
+        self.Storage_File = Storage_File
+        self.blocks_file_sink_0_0.open(self.Storage_File)
+
+    def get_Interpolation(self):
+        return self.Interpolation
+
+    def set_Interpolation(self, Interpolation):
+        self.Interpolation = Interpolation
+
+    def get_Demod_Decimation(self):
+        return self.Demod_Decimation
+
+    def set_Demod_Decimation(self, Demod_Decimation):
+        self.Demod_Decimation = Demod_Decimation
+
+    def get_Center_Frequency(self):
+        return self.Center_Frequency
+
+    def set_Center_Frequency(self, Center_Frequency):
+        self.Center_Frequency = Center_Frequency
+        self.soapy_limesdr_source_0_0.set_frequency(0, self.Center_Frequency)
+
+
+
+
+def main(top_block_cls=receive_data, options=None):
+    tb = top_block_cls()
+
+    def sig_handler(sig=None, frame=None):
+        tb.stop()
+        tb.wait()
+
+        sys.exit(0)
+
+    signal.signal(signal.SIGINT, sig_handler)
+    signal.signal(signal.SIGTERM, sig_handler)
+
+    tb.start()
+
+    tb.wait()
+
+
+if __name__ == '__main__':
+    main()
