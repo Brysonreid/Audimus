ac935b90a380fc4654cb44eea66cfba53c7cad97
Bryson Reid <19bwr@queensu.ca>
2025-04-18 13:10:11 -0400

    updates

--- UncompressFiles.py
+++ UncompressFiles.py
@@ -1,7 +1,18 @@
 import numpy as np
+import os
+import sys
 
-output_filename = 'Pi/Recordings/SDRdata2025-04-02-16_18_321'
+output_folder = sys.argv[1]
 
-iq_data2 = np.fromfile(open(output_filename), dtype=np.int8)
-float32=iq_data2.astype(np.int16)
-float32.tofile(output_filename)
+
+if os.path.exists(output_folder):
+
+    for output_filename in os.listdir(output_folder):
+        print(output_filename)
+        iq_data2 = np.fromfile(open(output_folder+"/"+output_filename), dtype=np.int8)
+        float32=iq_data2.astype(np.int16)
+        float32.tofile(output_folder+"/"+output_filename)
+        
+    print("Files Decompressed")
+else:
+    print(output_folder + " folder not found")
